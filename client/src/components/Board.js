40fea116aab3b94b99adcbbcc38f158f04799fbb
Jesse Cummings <jesse.cummings.1981@gmail.com>
2018-10-16 11:11:36 -0600

    added dice images and functionality to Game

--- client/src/components/Board.js
+++ client/src/components/Board.js
@@ -1,1 +1,18 @@
-export default () => null
+import React from 'react'
+import { Grid } from 'semantic-ui-react'
+import Dice from './Dice'
+
+const Board = () => (
+<Grid>
+    <Grid.Row>
+    <Dice value={1} />
+    <Dice value={2} />
+    <Dice value={3} />
+    <Dice value={4} />
+    <Dice value={5} />
+    </Grid.Row>
+</Grid>
+ 
+)
+
+export default Board
