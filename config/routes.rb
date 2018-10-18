688756e5ede5ad7b8408c3520b37a5e47fdb4576
Jesse Cummings <jesse.cummings.1981@gmail.com>
2018-10-18 12:29:10 -0600

    added seed file for high scores

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,6 @@ Rails.application.routes.draw do
   mount_devise_token_auth_for 'User', at: 'api/auth'
   namespace :api do
     resources :scores, only: [:index, :create]
-    #API ROUTES SHOULD GO HERE
   end
 
   #Do not place any routes below this one
